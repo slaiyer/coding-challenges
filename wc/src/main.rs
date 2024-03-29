e8a08e3a6cbbcf6189e255beb11be397e31902a8
slaiyer <1776970+slaiyer@users.noreply.github.com>
2024-03-29 13:06:31 +0530

    improve wc readability

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -3,7 +3,7 @@ use std::error;
 use std::fs;
 use std::io;
 
-enum Opts {
+enum Options {
     All,
     Lines,
     Words,
@@ -11,14 +11,14 @@ enum Opts {
     Chars,
 }
 
-impl Opts {
-    fn from_str(s: &str) -> Opts {
+impl Options {
+    fn from_str(s: &str) -> Options {
         match s {
-            "l" => Opts::Lines,
-            "w" => Opts::Words,
-            "c" => Opts::Bytes,
-            "m" => Opts::Chars,
-            _ => Opts::All,
+            "l" => Options::Lines,
+            "w" => Options::Words,
+            "c" => Options::Bytes,
+            "m" => Options::Chars,
+            _ => Options::All,
         }
     }
 }
@@ -28,43 +28,37 @@ const BUF_LEN: usize = 1_024_000;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let opts: Opts;
+    let options: Options;
 
     let mut reader_buffered: Box<dyn io::BufRead>;
     let stdin = io::stdin().lock();
 
     match args.len() {
         1 => {
-            opts = Opts::All;
+            options = Options::All;
             reader_buffered = Box::new(create_buffered_reader(BUF_LEN, stdin));
         }
         2 => {
             if &args[1][..1] != "-" {
-                opts = Opts::All;
+                options = Options::All;
                 let file = &args[1];
-                reader_buffered = Box::new(create_buffered_reader(
-                    BUF_LEN,
-                    fs::File::open(file)?,
-                ));
+                reader_buffered = Box::new(create_buffered_reader(BUF_LEN, fs::File::open(file)?));
             } else {
-                opts = Opts::from_str(&args[1][1..]);
+                options = Options::from_str(&args[1][1..]);
                 reader_buffered = Box::new(create_buffered_reader(BUF_LEN, stdin));
             }
         }
         3 => {
-            opts = Opts::from_str(&args[1][1..]);
+            options = Options::from_str(&args[1][1..]);
             let file = &args[2];
-            reader_buffered = Box::new(create_buffered_reader(
-                BUF_LEN,
-                fs::File::open(file)?,
-            ));
+            reader_buffered = Box::new(create_buffered_reader(BUF_LEN, fs::File::open(file)?));
         }
-        _ => return Err(From::from("invalid arguments"))
+        _ => return Err(From::from("invalid arguments")),
     }
 
-    match process(&mut reader_buffered, opts) {
+    match process(&mut reader_buffered, options) {
         Ok(_) => {}
-        Err(e) => eprintln!("{}", e)
+        Err(e) => eprintln!("{}", e),
     }
 
     if args.len() > 1 && &args[1][..1] != "-" {
@@ -76,7 +70,10 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
-fn process(reader: &mut Box<dyn io::BufRead>, opts: Opts) -> Result<(), Box<dyn error::Error>> {
+fn process(
+    reader: &mut Box<dyn io::BufRead>,
+    options: Options,
+) -> Result<(), Box<dyn error::Error>> {
     let mut lines = 0;
     let mut words = 0;
     let mut bytes = 0;
@@ -86,47 +83,39 @@ fn process(reader: &mut Box<dyn io::BufRead>, opts: Opts) -> Result<(), Box<dyn 
     loop {
         let n = reader.read(&mut buf)?;
         if n == 0 {
-            break
+            break;
         }
 
         let slice = std::str::from_utf8(&buf[..n])?;
 
-        match opts {
-            Opts::All => {
+        match options {
+            Options::All => {
                 bytes += n;
                 lines += count_lines(slice);
                 words += count_words(slice);
                 chars += count_chars(slice);
             }
-            Opts::Lines => {
-                lines += count_lines(slice)
-            }
-            Opts::Words => {
-                words += count_words(slice)
-            }
-            Opts::Bytes => {
-                bytes += n
-            }
-            Opts::Chars => {
-                chars += count_chars(slice)
-            }
+            Options::Lines => lines += count_lines(slice),
+            Options::Words => words += count_words(slice),
+            Options::Bytes => bytes += n,
+            Options::Chars => chars += count_chars(slice),
         }
     }
 
-    match opts {
-        Opts::All => {
+    match options {
+        Options::All => {
             print!("{:>8}{:>8}{:>8}{:>8}", lines, words, bytes, chars)
         }
-        Opts::Lines => {
+        Options::Lines => {
             print!("{:>8}", lines)
         }
-        Opts::Words => {
+        Options::Words => {
             print!("{:>8}", words)
         }
-        Opts::Bytes => {
+        Options::Bytes => {
             print!("{:>8}", bytes)
         }
-        Opts::Chars => {
+        Options::Chars => {
             print!("{:>8}", chars)
         }
     }
@@ -134,18 +123,18 @@ fn process(reader: &mut Box<dyn io::BufRead>, opts: Opts) -> Result<(), Box<dyn 
     Ok(())
 }
 
-fn count_lines(slice: &str) -> usize {
-    slice.lines().count()
+fn count_lines(s: &str) -> usize {
+    s.lines().count()
 }
 
-fn count_words(slice: &str) -> usize {
-    slice.split_whitespace().count()
+fn count_words(s: &str) -> usize {
+    s.split_whitespace().count()
 }
 
-fn count_chars(slice: &str) -> usize {
-    slice.chars().count()
+fn count_chars(s: &str) -> usize {
+    s.chars().count()
 }
 
-fn create_buffered_reader<R: io::Read>(buf_len: usize, input: R) -> impl io::BufRead {
-    io::BufReader::with_capacity(buf_len, input)
+fn create_buffered_reader<R: io::Read>(capacity: usize, input: R) -> impl io::BufRead {
+    io::BufReader::with_capacity(capacity, input)
 }
