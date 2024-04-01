c5a5c87e267d079f83f83eae8b8565dc1c78edae
slaiyer <1776970+slaiyer@users.noreply.github.com>
2024-04-01 22:52:35 +0530

    redis-server rust: use dashmap instead of hashmap for concurrent performance

--- redis-server/src/kvstore.rs
+++ redis-server/src/kvstore.rs
@@ -1,36 +1,31 @@
-use std::collections::HashMap;
-use std::sync::Mutex;
+use dashmap::DashMap;
 use lazy_static::lazy_static;
 
 pub struct KvStore {
-    store: Mutex<HashMap<String, String>>,
+    store: DashMap<String, String>,
 }
 
 impl KvStore {
     fn new() -> Self {
         Self {
-            store: Mutex::new(HashMap::new()),
+            store: DashMap::new(),
         }
     }
 
     pub fn exists(&self, key: String) -> bool {
-        let store = self.store.lock().unwrap();
-        store.contains_key(&key)
+        self.store.contains_key(&key)
     }
 
     pub fn set(&self, key: String, value: String) {
-        let mut store = self.store.lock().unwrap();
-        store.insert(key, value);
+        self.store.insert(key, value);
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        let store = self.store.lock().unwrap();
-        store.get(&key).cloned()
+        self.store.get(&key).map(|value_ref| value_ref.clone())
     }
 
     pub fn del(&self, key: String) -> Option<String> {
-        let mut store = self.store.lock().unwrap();
-        store.remove(&key)
+        self.store.remove(&key).map(|(_, v)| v)
     }
 }
 
