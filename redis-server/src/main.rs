2900d39f977497aea285d5c0f34aa3e8c81cdc7d
slaiyer <1776970+slaiyer@users.noreply.github.com>
2024-04-11 23:41:01 +0530

    redis-server rust: refactor, add tests

--- redis-server/src/main.rs
+++ redis-server/src/main.rs
@@ -3,8 +3,6 @@
 
 use std::error;
 
-use command::types::{Command, Execute};
-use response::types::Response;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::spawn;
@@ -72,7 +70,7 @@ fn process(request_buf: &[u8]) -> String {
         Err(error) => return error.to_string(),
     };
 
-    let commands = match parse_commands(&request) {
+    let commands = match deserialize::parse_commands(&request) {
         Ok(value) => value,
         Err(value) => return value,
     };
@@ -81,96 +79,4 @@ fn process(request_buf: &[u8]) -> String {
         .into_iter()
         .map(|command| command.execute().to_string())
         .collect()
-}
-
-fn parse_commands(request: &Request) -> Result<Vec<Box<dyn Execute>>, String> {
-    let mut commands: Vec<Box<dyn Execute>> = Vec::new();
-    for cmd in request.commands() {
-        let cmd_type = match cmd.first() {
-            Some(cmd) => match cmd.parse::<Command>() {
-                Ok(result) => result,
-                Err(error) => return Err(error.to_string()),
-            },
-            None => return Err(Response::err("", "empty command").to_string()),
-        };
-
-        commands.push(match cmd_type {
-            Command::Ping(builder) => match cmd.len() {
-                1 => Box::new(builder.build()),
-                2 => Box::new(builder.message(cmd[1].as_str()).build()),
-                _ => {
-                    return Err(
-                        Response::err("", "unexpected number of arguments for PING").to_string()
-                    )
-                }
-            },
-            Command::Echo(builder) => match cmd.len() {
-                2 => match builder.message(cmd[1].as_str()).build() {
-                    Ok(result) => Box::new(result),
-                    Err(error) => return Err(error.to_string()),
-                },
-                _ => {
-                    return Err(
-                        Response::err("", "unexpected number of arguments for ECHO").to_string()
-                    )
-                }
-            },
-            Command::Exists(builder) => match cmd.len() {
-                2 => match builder.key(cmd[1].as_str()).build() {
-                    Ok(result) => Box::new(result),
-                    Err(error) => return Err(error.to_string()),
-                },
-                _ => {
-                    return Err(
-                        Response::err("", "unexpected number of arguments for EXISTS").to_string(),
-                    )
-                }
-            },
-            Command::Config(builder) => match cmd.len() {
-                3 => match builder.args(cmd[1..].to_vec()).build() {
-                    Ok(result) => Box::new(result),
-                    Err(error) => return Err(error.to_string()),
-                },
-                _ => {
-                    return Err(
-                        Response::err("", "unexpected number of arguments for CONFIG").to_string(),
-                    )
-                }
-            },
-            Command::Set(builder) => match cmd.len() {
-                3 => match builder.key(cmd[1].as_str()).value(cmd[2].as_str()).build() {
-                    Ok(result) => Box::new(result),
-                    Err(error) => return Err(error.to_string()),
-                },
-                _ => {
-                    return Err(
-                        Response::err("", "unexpected number of arguments for SET").to_string(),
-                    )
-                }
-            },
-            Command::Get(builder) => match cmd.len() {
-                2 => match builder.key(cmd[1].as_str()).build() {
-                    Ok(result) => Box::new(result),
-                    Err(error) => return Err(error.to_string()),
-                },
-                _ => {
-                    return Err(
-                        Response::err("", "unexpected number of arguments for GET").to_string(),
-                    )
-                }
-            },
-            Command::Del(builder) => match cmd.len() {
-                2 => match builder.key(cmd[1].as_str()).build() {
-                    Ok(result) => Box::new(result),
-                    Err(error) => return Err(error.to_string()),
-                },
-                _ => {
-                    return Err(
-                        Response::err("", "unexpected number of arguments for DEL").to_string(),
-                    )
-                }
-            },
-        });
-    }
-    Ok(commands)
 }
--- redis-server/src/request/deserialize.rs
+++ redis-server/src/request/deserialize.rs
@@ -1,4 +1,6 @@
-use crate::response::types::Response;
+use crate::{command::types::{Command, Execute}, response::types::Response};
+
+use super::types::Request;
 
 pub fn stringify(request_buf: &[u8]) -> Result<String, Response> {
     let request_str: String = match std::str::from_utf8(request_buf) {
@@ -7,6 +9,98 @@ pub fn stringify(request_buf: &[u8]) -> Result<String, Response> {
     };
 
     Ok(request_str.trim_matches('\0').into())
+}
+
+pub fn parse_commands(request: &Request) -> Result<Vec<Box<dyn Execute>>, String> {
+    let mut commands: Vec<Box<dyn Execute>> = Vec::new();
+    for cmd in request.commands() {
+        let cmd_type = match cmd.first() {
+            Some(cmd) => match cmd.parse::<Command>() {
+                Ok(result) => result,
+                Err(error) => return Err(error.to_string()),
+            },
+            None => return Err(Response::err("", "empty command").to_string()),
+        };
+
+        commands.push(match cmd_type {
+            Command::Ping(builder) => match cmd.len() {
+                1 => Box::new(builder.build()),
+                2 => Box::new(builder.message(cmd[1].as_str()).build()),
+                _ => {
+                    return Err(
+                        Response::err("", "unexpected number of arguments for PING").to_string()
+                    )
+                }
+            },
+            Command::Echo(builder) => match cmd.len() {
+                2 => match builder.message(cmd[1].as_str()).build() {
+                    Ok(result) => Box::new(result),
+                    Err(error) => return Err(error.to_string()),
+                },
+                _ => {
+                    return Err(
+                        Response::err("", "unexpected number of arguments for ECHO").to_string()
+                    )
+                }
+            },
+            Command::Exists(builder) => match cmd.len() {
+                2 => match builder.key(cmd[1].as_str()).build() {
+                    Ok(result) => Box::new(result),
+                    Err(error) => return Err(error.to_string()),
+                },
+                _ => {
+                    return Err(
+                        Response::err("", "unexpected number of arguments for EXISTS").to_string(),
+                    )
+                }
+            },
+            Command::Config(builder) => match cmd.len() {
+                3 => match builder.args(cmd[1..].to_vec()).build() {
+                    Ok(result) => Box::new(result),
+                    Err(error) => return Err(error.to_string()),
+                },
+                _ => {
+                    return Err(
+                        Response::err("", "unexpected number of arguments for CONFIG").to_string(),
+                    )
+                }
+            },
+            Command::Set(builder) => match cmd.len() {
+                3 => match builder.key(cmd[1].as_str()).value(cmd[2].as_str()).build() {
+                    Ok(result) => Box::new(result),
+                    Err(error) => return Err(error.to_string()),
+                },
+                _ => {
+                    return Err(
+                        Response::err("", "unexpected number of arguments for SET").to_string(),
+                    )
+                }
+            },
+            Command::Get(builder) => match cmd.len() {
+                2 => match builder.key(cmd[1].as_str()).build() {
+                    Ok(result) => Box::new(result),
+                    Err(error) => return Err(error.to_string()),
+                },
+                _ => {
+                    return Err(
+                        Response::err("", "unexpected number of arguments for GET").to_string(),
+                    )
+                }
+            },
+            Command::Del(builder) => match cmd.len() {
+                2 => match builder.key(cmd[1].as_str()).build() {
+                    Ok(result) => Box::new(result),
+                    Err(error) => return Err(error.to_string()),
+                },
+                _ => {
+                    return Err(
+                        Response::err("", "unexpected number of arguments for DEL").to_string(),
+                    )
+                }
+            },
+        });
+    }
+    Ok(commands)
 }
 
 #[cfg(test)]
@@ -53,5 +147,15 @@ mod tests {
         let request_buf = b"\0\0ping \r\n\0\0";
         let result = stringify(request_buf);
         assert_eq!(result, Ok("ping \r\n".into()));
+    }
+
+    #[test]
+    fn test_parse_commands_echo() {
+        let request = "echo ling\r\n".parse::<Request>().unwrap();
+        let result = parse_commands(&request);
+        assert!(result.is_ok());
+        let commands = result.unwrap();
+        assert_eq!(commands.len(), 1);
+        assert_eq!(commands.into_iter().map(|cmd| cmd.execute().to_string()).collect::<String>(), "+ling\r\n");
     }
 }
